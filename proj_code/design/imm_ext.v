816e3966dd4af1d3898d98cd3dac2c8d7a6a193e
D'Arcy Anderson <darcy.anderson@nyu.edu>
2022-12-07 15:04:34 -0500

    Implemented immediate extension module

---
+++ proj_code/design/imm_ext.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+module imm_ext(
+  input [1:0] imm_ext_en, // Control signal
+  input [19:0] imm_in, // Non extended input value
+  output [31:0] imm_out // Extended output value
+);
+
+  assign imm_out = (imm_ext_en == 2'b00) ? {{20{imm_in[11]}}, imm_in[11:0]} : // Signed 12-bit
+                   (imm_ext_en == 2'b01) ? {{12{imm_in[19]}}, imm_in[19:0]} : // Signed 20-bit
+                   (imm_ext_en == 2'b10) ? {20'b0, imm_in[11:0]} : // Unsigned 12-bit
+                   (imm_ext_en == 2'b11) ? {12'b0, imm_in[19:0]} : // Unsigned 20-bit
+                   32'bx;
+
+endmodule
