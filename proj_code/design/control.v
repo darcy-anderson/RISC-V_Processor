96e8df4d29f78e7eb015d35d59cdea45dde518aa
9nature <koo.jenny.22@gmail.com>
2022-12-07 22:04:11 -0500

    testing connections

--- proj_code/design/control.v
+++ proj_code/design/control.v
@@ -11,12 +11,12 @@
 
 
 module control(
-    input clk,
-    input [31:0] inst,
-    input aluEq, // for branch equals operations
+    input wire clk,
+    input wire [31:0] inst,
+    input wire aluEq, // for branch equals operations
     output reg regWrite,
-    output reg memRead, // TODO: modify to 2 bits for word/half/byte access
-    output reg memWrite, // TODO: modify to 2 bits
+    output reg [3:0] memControl, // TODO: modify to 2 bits for word/half/byte access
+//    output reg memWrite, // TODO: modify to 2 bits
     output reg memToReg, // 0 for ALU result, 1 for mem data
     output reg aluSrc, // 0 for reg data, 1 for imm
     output reg [3:0] aluOp, // usually generated from 
@@ -30,114 +30,114 @@ module control(
     always @ (clk, inst) begin
     
     case (instOpcode) 
-        `OP_LUI: begin 
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            aluSrc = 1'b1;
-            aluOp = `EXE_ADD_OP; // add upper immediate 
-            branch = 1'b0;        
-        end
-        `OP_AUIPC: begin
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            aluSrc = 1'b1;
-            aluOp = `EXE_ADD_OP;
-            branch = 1'b1; // change pc     
-        end
-        `OP_JAL: begin
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            aluSrc = 1'b0;
-            aluOp = `EXE_JAL_OP;
-            branch = 1'b1; // change pc     
-        end
-        `OP_JALR: begin
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            aluSrc = 1'b0;
-            aluOp = `EXE_JALR_OP;
-            branch = 1'b1; // change pc     
-        end
-        `OP_BRANCH: begin // Missing - IMM value sent to sign extension module for pc counter handling
-            case(funct3)
-                `FUNCT3_BEQ : begin
-                    aluOp = `EXE_BEQ_OP;
-                end
-                `FUNCT3_BNE : begin
-                    aluOp = `EXE_BNE_OP;
-                end
-                `FUNCT3_BLT : begin
-                    aluOp = `EXE_BLT_OP;
-                end
-                `FUNCT3_BGE : begin
-                    aluOp = `EXE_BGE_OP;
-                end 
-                `FUNCT3_BLTU : begin
-                    aluOp = `EXE_BLTU_OP;
-                end
-                `FUNCT3_BGEU : begin
-                    aluOp = `EXE_BGEU_OP;
-                end
-            endcase
-            aluSrc = 1'b0;
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            branch = 1'b1; // change pc   
-        end
-        `OP_LOAD: begin // TODO: info handling for sign extension module, and 
-            case(funct3) 
-                `FUNCT3_LB : begin
-                    aluOp = `EXE_LB_OP;
-                end
-                `FUNCT3_LH : begin
-                    aluOp = `EXE_LH_OP;
-                end
-                `FUNCT3_LW : begin
-                    aluOp = `EXE_LW_OP;
-                end
-                `FUNCT3_LBU : begin
-                    aluOp = `EXE_LBU_OP;
-                end 
-                `FUNCT3_LHU : begin
-                    aluOp = `EXE_LHU_OP;
-                end
-            endcase
-            regWrite = 1'b1;
-            memRead = 1'b1; // TODO: update to 2 bits and set different values for each funct3, or add a memory control unit to mask unwanted values
-            memWrite = 1'b0;
-            memToReg = 1'b1;
-            aluSrc = 1'b0;
-            branch = 1'b0;     
-        end         
-        `OP_STORE: begin            
-        case(funct3) 
-                `FUNCT3_SB : begin
-                    aluOp = `EXE_SB_OP;
-                end
-                `FUNCT3_SH : begin
-                    aluOp = `EXE_SH_OP;
-                end
-                `FUNCT3_SW : begin
-                    aluOp = `EXE_SW_OP;
-                end
-            endcase
-            regWrite = 1'b0;
-            memRead = 1'b0;
-            memWrite = 1'b1; // TODO:
-            memToReg = 1'b0;
-            aluSrc = 1'b1;
-            branch = 1'b0;     
-        end        
+//        `OP_LUI: begin 
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            aluSrc = 1'b1;
+//            aluOp = `EXE_ADD_OP; // add upper immediate 
+//            branch = 1'b0;        
+//        end
+//        `OP_AUIPC: begin
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            aluSrc = 1'b1;
+//            aluOp = `EXE_ADD_OP;
+//            branch = 1'b1; // change pc     
+//        end
+//        `OP_JAL: begin
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            aluSrc = 1'b0;
+//            aluOp = `EXE_JAL_OP;
+//            branch = 1'b1; // change pc     
+//        end
+//        `OP_JALR: begin
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            aluSrc = 1'b0;
+//            aluOp = `EXE_JALR_OP;
+//            branch = 1'b1; // change pc     
+//        end
+//        `OP_BRANCH: begin // Missing - IMM value sent to sign extension module for pc counter handling
+//            case(funct3)
+//                `FUNCT3_BEQ : begin
+//                    aluOp = `EXE_BEQ_OP;
+//                end
+//                `FUNCT3_BNE : begin
+//                    aluOp = `EXE_BNE_OP;
+//                end
+//                `FUNCT3_BLT : begin
+//                    aluOp = `EXE_BLT_OP;
+//                end
+//                `FUNCT3_BGE : begin
+//                    aluOp = `EXE_BGE_OP;
+//                end 
+//                `FUNCT3_BLTU : begin
+//                    aluOp = `EXE_BLTU_OP;
+//                end
+//                `FUNCT3_BGEU : begin
+//                    aluOp = `EXE_BGEU_OP;
+//                end
+//            endcase
+//            aluSrc = 1'b0;
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            branch = 1'b1; // change pc   
+//        end
+//        `OP_LOAD: begin // TODO: info handling for sign extension module, and 
+//            case(funct3) 
+//                `FUNCT3_LB : begin
+//                    aluOp = `EXE_LB_OP;
+//                end
+//                `FUNCT3_LH : begin
+//                    aluOp = `EXE_LH_OP;
+//                end
+//                `FUNCT3_LW : begin
+//                    aluOp = `EXE_LW_OP;
+//                end
+//                `FUNCT3_LBU : begin
+//                    aluOp = `EXE_LBU_OP;
+//                end 
+//                `FUNCT3_LHU : begin
+//                    aluOp = `EXE_LHU_OP;
+//                end
+//            endcase
+//            regWrite = 1'b1;
+//            memRead = 1'b1; // TODO: update to 2 bits and set different values for each funct3, or add a memory control unit to mask unwanted values
+//            memWrite = 1'b0;
+//            memToReg = 1'b1;
+//            aluSrc = 1'b0;
+//            branch = 1'b0;     
+//        end         
+//        `OP_STORE: begin            
+//        case(funct3) 
+//                `FUNCT3_SB : begin
+//                    aluOp = `EXE_SB_OP;
+//                end
+//                `FUNCT3_SH : begin
+//                    aluOp = `EXE_SH_OP;
+//                end
+//                `FUNCT3_SW : begin
+//                    aluOp = `EXE_SW_OP;
+//                end
+//            endcase
+//            regWrite = 1'b0;
+//            memRead = 1'b0;
+//            memWrite = 1'b1; // TODO:
+//            memToReg = 1'b0;
+//            aluSrc = 1'b1;
+//            branch = 1'b0;     
+//        end        
         `OP_ALU: begin
         case(funct3)
                 `FUNCT3_ADD_SUB : begin // sub == add negative value
@@ -172,50 +172,49 @@ module control(
                 end
             endcase
             regWrite = 1'b1;
-            memRead = 1'b0;
-            memWrite = 1'b0;
+            memControl = 3'b000;
             memToReg = 1'b0;
             aluSrc = 1'b0;
             branch = 1'b0;      
         end        
-        `OP_ALU_IMM: begin
-        case(funct3)
-                `FUNCT3_ADDI : begin // sub == add negative value
-                    aluOp = `EXE_SLL_OP;
-                end
-                `FUNCT3_SLLI : begin
-                    aluOp = `EXE_SLL_OP;
-                end
-                `FUNCT3_SLTI : begin
-                    aluOp = `EXE_SLT_OP;
-                end
-                `FUNCT3_SLTIU : begin
-                    aluOp = `EXE_SLTU_OP;
-                end 
-                `FUNCT3_XORI : begin
-                    aluOp = `EXE_XOR_OP;
-                end
-                `FUNCT3_SRL_SRA : begin
-                    case(funct7)
-                        `FUNCT7_SRL: aluOp = `EXE_SRL_OP;
-                        `FUNCT7_SRA: aluOp = `EXE_SRA_OP;
-                    endcase
-                end 
-                `FUNCT3_ORI : begin
-                    aluOp = `EXE_OR_OP;
-                end
-                `FUNCT3_ANDI : begin
-                    aluOp = `EXE_AND_OP;
-                end
-            endcase
-            regWrite = 1'b1;
-            memRead = 1'b0;
-            memWrite = 1'b0;
-            memToReg = 1'b0;
-            aluSrc = 1'b1;
-            branch = 1'b0;     
-        end
-        default: $diplay("incorrect opcode");
+//        `OP_ALU_IMM: begin
+//        case(funct3)
+//                `FUNCT3_ADDI : begin // sub == add negative value
+//                    aluOp = `EXE_SLL_OP;
+//                end
+//                `FUNCT3_SLLI : begin
+//                    aluOp = `EXE_SLL_OP;
+//                end
+//                `FUNCT3_SLTI : begin
+//                    aluOp = `EXE_SLT_OP;
+//                end
+//                `FUNCT3_SLTIU : begin
+//                    aluOp = `EXE_SLTU_OP;
+//                end 
+//                `FUNCT3_XORI : begin
+//                    aluOp = `EXE_XOR_OP;
+//                end
+//                `FUNCT3_SRL_SRA : begin
+//                    case(funct7)
+//                        `FUNCT7_SRL: aluOp = `EXE_SRL_OP;
+//                        `FUNCT7_SRA: aluOp = `EXE_SRA_OP;
+//                    endcase
+//                end 
+//                `FUNCT3_ORI : begin
+//                    aluOp = `EXE_OR_OP;
+//                end
+//                `FUNCT3_ANDI : begin
+//                    aluOp = `EXE_AND_OP;
+//                end
+//            endcase
+//            regWrite = 1'b1;
+//            memRead = 1'b0;
+//            memWrite = 1'b0;
+//            memToReg = 1'b0;
+//            aluSrc = 1'b1;
+//            branch = 1'b0;     
+//        end
+        default: $display("incorrect opcode");
     endcase
     
     end
--- proj_code/design/program_counter.v
+++ proj_code/design/program_counter.v
@@ -5,17 +5,19 @@
 
 module program_counter(
     input wire clk,
+    input wire en,
     input wire rst,
     input wire [31:0] c_in,
     output wire [31:0] c_out
     );
 reg [31:0] i_out;
+
 always @(posedge clk)
 begin
-    if (rst == 1)
-        i_out <= 32'h01000000;
-    else 
+    if (en == 1) 
         i_out <= c_in;
+    else if (rst == 1)
+        i_out <= 32'h01000000;
 end
 
 assign c_out = i_out;
