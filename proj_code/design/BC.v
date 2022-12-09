3dd3940a7f483a486cb704010003cd432bd6493c
IrvingF7 <zichuanfang2015@yahoo.com>
2022-12-08 20:10:51 -0500

    fixed some mistakes with ALU and BC

---
+++ proj_code/design/BC.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+`default_nettype none
+`include "const.v"
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module BC(
+    input wire [31:0] rs1,
+    input wire [31:0] rs2,
+    input wire [3:0] opcode,
+    output wire out
+    );
+    
+reg result;
+
+always@(*) begin
+    case(opcode)
+        `EXE_BEQ_OP:
+            result <= (rs1==rs2) ? 1 : 0;
+        `EXE_BNE_OP:
+            result <= (rs1==rs2) ? 0 : 1;
+        `EXE_BLT_OP:
+            result <= ($signed(rs1) < $signed(rs2)) ? 1 : 0;
+        `EXE_BGE_OP:
+            result <= ($signed(rs1) < $signed(rs2)) ? 0 : 1;
+        `EXE_BLTU_OP:
+            result <= (rs1 < rs2)? 1 : 0;
+        `EXE_BGEU_OP:
+            result <= (rs1 < rs2)? 0 : 1;
+         default:
+            result <= result;
+    endcase
+end
+
+assign out = result;
+endmodule
